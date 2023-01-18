cee8deb16b7758daffcad9abf7a787df0d5bfaf6
Dominik <dominikant99@gmail.com>
2023-01-18 16:26:08 +0100

    Change method to create new grids, add init grid size

--- scripts.js
+++ scripts.js
@@ -1,34 +1,50 @@
 const board = document.querySelector('.board');
-
-
-for(let i = 0; i < 16 * 16; i++) {
-    const cell = document.createElement('div');
-    cell.classList.toggle('cell');
-    board.appendChild(cell);
-}
-
 const cells = document.querySelectorAll('.cell');
 
-// board.addEventListener('mouseover', (e) => {
-//     if (e.target.matches('.cell')) {
-//         e.target.classList.add('active');
-//         e.target.style.backgroundColor = "white";
-//     }
-// })
-
-console.log(cells)
-
-
-// cells.forEach(cell => {
-//     cell.addEventListener('mouseover', (e) => {
-//         if(e.target.matches('.cell')) {
-//             e.target.style.backgroundColor = "white";
-//         }
-//     })
-// })
-
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', (e) => {
+initGrid = () => {
+    for(let i = 0; i < 256; i++) {
+        const cell = document.createElement('div');
+        cell.classList.toggle('cell');
+        cell.addEventListener('mouseover', (e) => {
         e.target.style.backgroundColor = 'white'
         })
+        board.appendChild(cell);
+}
+};
+
+function newGridSize(val) {
+    clearBoard();
+    createDivs(val);
+    board.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${val}, 1fr)`;
+}
+
+function createDivs(size) {
+    for(let i = 0; i < size * size; i++) {
+        const cell = document.createElement('div');
+        cell.classList.toggle('cell');
+        cell.style.width = '700px / size'
+        cell.style.height = '700px / size'
+        cell.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'white'
+        })
+        board.appendChild(cell);
+    }
+}
+
+function clearBoard() {
+    while(board.firstChild) {
+        board.removeChild(board.firstChild)
+    }
+}
+
+const slider = document.querySelector('#slider')
+const textValue = document.querySelector('.value')
+
+slider.addEventListener('input', function() {
+    let val = document.getElementById('slider').value;
+    textValue.textContent = val;
+    newGridSize(val)
 })
+
+initGrid();
